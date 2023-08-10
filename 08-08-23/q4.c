d82114012bf5d7a68522d84a160a8e2c5e4c3a3b
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-08-10 21:57:18 +0530

    Assignment 1

---
+++ 08-08-23/q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+void fileSizeLSEEK(char **filename) {
+    int fd = open(filename, O_RDONLY);
+    int size = lseek(fd, 0, SEEK_END);
+
+    printf("LSEEK Size of %s = %d\n", filename, size);
+    close(fd);
+}
+
+void fileSizeSTAT(char **filename) {
+    struct stat stbuf;
+    stat(filename, &stbuf);
+
+    printf("STAT Size of %s = %d\n", filename, stbuf.st_size);
+    printf("BLOCK Size of %s = %d\n", filename, stbuf.st_blocks);
+    printf("BLOCK Size of %s = %d\n", filename, stbuf.st_ino);
+}
+
+int main(int argc, char **argv) {
+    fileSizeLSEEK(argv[1]);
+    fileSizeSTAT(argv[1]);
+    return 0;
+}
