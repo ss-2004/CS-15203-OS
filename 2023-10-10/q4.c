fe47d47302d39e6ef949df296234461fec64702a
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-10-10 18:42:13 +0530

    Assignment 5

--- 2023-10-10/q4.c
+++ 2023-10-10/q4.c
@@ -1,19 +1,21 @@
+// Q4
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 int main() {
     sigset_t blk_msk;
     sigemptyset(&blk_msk);
     sigaddset(&blk_msk, SIGINT);
 
-    if(sigprocmask(SIG_BLOCK, &blk_msk, NULL) == -1) {
+    if (sigprocmask(SIG_BLOCK, &blk_msk, NULL) == -1) {
         perror("sigprocmask\n");
         return 1;
     }
 
     printf("SIGINT signal ^C is blocked! Press ^C to test.\n");
-    while(1){
+    while (1) {
         printf("Performing random bg tasks...\n\n");
         sleep(2);
     }
